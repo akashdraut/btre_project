817414db8e803487eaf0b2aace71b1880bad9244
Akash <adraut93@gmail.com>
2020-04-30 10:54:54 +0530

    Created user database model in pages app. Added user model manager. Set the custom user model and run the migration command with the database sqlite3

--- btre/settings.py
+++ btre/settings.py
@@ -80,10 +80,29 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'btre_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'admin@123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
+    #
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'btredb',
+    #     'USER': 'root',
+    #     'PASSWORD': 'admin@123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
 }
 
 
@@ -128,3 +147,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+AUTH_USER_MODEL = 'pages.UserProfile'
