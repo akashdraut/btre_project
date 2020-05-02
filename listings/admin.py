41300d2c59c216f010358287275fb775aff54df7
Akash <adraut93@gmail.com>
2020-05-02 13:22:34 +0530

    Pull data from the listings model

--- listings/admin.py
+++ listings/admin.py
@@ -9,7 +9,8 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')  # To make clickable link (title)
     list_filter = ('realtor',)  # To filter according to the realtors
     list_editable = ('is_published',)  # To make it editable (CheckBox)
-    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')  # Searchable fields
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')  # This will create search
+    # box where we can search keyword from the fileds mentioned.
     list_per_page = 25  # For listings per page
 
 
