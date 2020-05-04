e8d311a2f2bb71c673b3e553f48e89d6ecd173db
Akash <adraut93@gmail.com>
2020-05-04 14:07:15 +0530

    listings/views.py modified for dynamic contents for single property information.

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 
 def index(request):
+    """ Contains all the published listings and pagination logic """
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 6)
@@ -16,8 +17,15 @@ def index(request):
     }
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    """ Contains single property information """
+    listing = get_object_or_404(Listing, pk=listing_id)  # if invalid id passed page_not_found error will show
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
