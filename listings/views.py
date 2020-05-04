474d5d81f6d0eaa9e263112ae60f973ca1813e62
Akash <adraut93@gmail.com>
2020-05-04 15:51:42 +0530

    listings.choices.py file created which contains state codes, beroom code and price code dictionaries. Search form with choices created.

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
+from .choices import *
+
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
@@ -28,4 +30,12 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    """ Contains code for search bar """
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
