f0285ce0cfd457416d0f5628bd2abcf0e54ab438
Akash <adraut93@gmail.com>
2020-05-04 11:45:48 +0530

    pages/views.py modified for dynamic contents on home and about page.

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    """ Contains & manage the data need to show on Featured Listing page """
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    """ Contains and manages the data need to show on About pag """
+
+    # Get all realtors based on hire_date
+    realtors = Realtor.objects.order_by('-hired_date')
+
+    # get mvp realtor
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
